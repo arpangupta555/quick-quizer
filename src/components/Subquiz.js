916789c529a5951f9a82bf59e29f5324acabdad9
ArpanGupta707 <arpangupta707@gmail.com>
2022-10-12 22:26:35 +0600

    10th commit

--- src/components/Subquiz.js
+++ src/components/Subquiz.js
@@ -15,9 +15,9 @@ const Subquiz = ({ quizs }) => {
     const notify = () => toast(correctAnswer);
 
 
-    console.log(option);
+    // const Sinoption = options.map(option => ({ option })
 
-
+    // console.log(Sinoption)
 
     return (
 
@@ -35,21 +35,53 @@ const Subquiz = ({ quizs }) => {
 
 
                 <div className="radio-buttons">
-                    {options.map(option => <div > <input
-                        id="windows"
-                        value="windows"
-                        name="platform"
+                    {options.map(option => <div >  <input
                         type="radio"
+                        name="radio"
+                        onChange={() => {
+                            if (correctAnswer === option) {
 
-
+                                toast("Your Answer is Right!");
+                            } else {
+                                toast("Sorry! Your Answer is wrong!");
+                            }
+                        }}
+                        className="radio checked:bg-red-500"
                     /> {option} </div>)}
-
+                    <ToastContainer />
 
 
 
                 </div>
 
 
+
+                {/* <div className="options grid md:grid-cols-2 gap-4 p-4 text-left">
+
+                    {quizs.options.map((option, index) => (
+                        <div className="form-control" key={index}>
+
+                            <label className="label justify-start cursor-pointer">
+                                <input
+                                    type="radio"
+                                    name="radio"
+                                    onChange={() => {
+                                        if (quizs.correctAnswer === option) {
+                                            setCount(count + 1);
+                                            toast("Your Answer is Right!");
+                                        } else {
+                                            toast("Sorry! Your Answer is wrong!");
+                                        }
+                                    }}
+                                    className="radio checked:bg-red-500"
+                                />
+                                <span className="label-text ml-2">{option}</span>
+                            </label>
+
+                            <ToastContainer />
+                        </div>
+                    )};
+                </div> */}
 
 
 
